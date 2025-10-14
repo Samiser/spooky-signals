2e0269cc9635966e8a079b103773ec64d3bc8f10
Samiser <github@me.samiser.xyz>
2025-10-14 15:35:51 +0000

    refined signal data layout and tested storing them in files

--- resources/signal_data.gd
+++ resources/signal_data.gd
@@ -3,22 +3,25 @@ class_name SignalData
 
 @export var title: String = "Unknown Transmission"
 
+@export_category("Content")
 @export var sender: String
-
 @export_multiline var content_text: String
+@export var content_image: Texture2D
 
+@export_category("Decoding Fields")
+# ideally generate from content but for now use cyberchef
 @export_multiline var encoded_content: String
 
+# again ideally this should be generated but manual for now
 @export var start_address: String
 @export var end_address: String
 
-@export var content_image: Texture2D
-
-# ideally randomly generate this but for now its just exported
-@export var target_coarse_frequency := 5678
-@export var target_cycles := 2.25
-@export var target_amplitude := 0.7
-@export var target_phase := 0.35
+# TODO: randomise these values
+var target_coarse_frequency := 5678
+var target_cycles := 2.25
+var target_amplitude := 0.7
+var target_phase := 0.35
+# ---
 
 var download_progress: float = 0.0 # / 100.0
 var current_download_speed: float = 0.0
