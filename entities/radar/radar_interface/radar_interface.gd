bf61a7bc5e9d7a9ffbbbf5c432141766a8a56dfa
wookbee <159189510+wookbee@users.noreply.github.com>
2025-10-15 23:05:05 +0100

    added radar ping

--- entities/radar/radar_interface/radar_interface.gd
+++ entities/radar/radar_interface/radar_interface.gd
@@ -2,3 +2,14 @@
 extends Node3D
 
 @onready var sprite: Sprite3D = $Sprite3D
+var ping_rate := 3.0
+var ping_time := 0.0
+
+func _process(delta: float) -> void:
+	ping_time += delta
+	if ping_time > ping_rate:
+		sprite.modulate = Color.WHITE
+		ping_time = 0.0
+		$AudioStreamPlayer3D.play()
+	else:
+		sprite.modulate = Color.WHITE * (ping_rate - ping_time)
