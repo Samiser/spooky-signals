ed87217d267a4527e0b6a9c36dc78642e7dd0395
Samiser <github@me.samiser.xyz>
2025-10-15 21:28:37 +0000

    added options menu

---
+++ ui/options_menu/volume_slider.gd
@@ -0,0 +1,12 @@
+extends HSlider
+
+@export var audio_bus_name := "Master"
+
+@onready var _bus := AudioServer.get_bus_index(audio_bus_name)
+
+func _ready() -> void:
+	value = db_to_linear(AudioServer.get_bus_volume_db(_bus))
+	value_changed.connect(_on_value_changed)
+
+func _on_value_changed(v: float) -> void:
+	AudioServer.set_bus_volume_db(_bus, linear_to_db(v))
