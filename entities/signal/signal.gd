c63924690cc36aa1d5e589509754e2976449e66d
wookbee <159189510+wookbee@users.noreply.github.com>
2025-10-16 19:13:14 +0100

    added moving signal support

--- entities/signal/signal.gd
+++ entities/signal/signal.gd
@@ -3,6 +3,11 @@ class_name SignalSource
 
 @export var data: SignalData
 
+@export var movement_positions: Array[Vector2]
+var current_movement_position = 0
+
+@onready var move_timer: Timer = $MoveTimer
+
 var active: bool = false: 
 	set(value):
 		active = value
@@ -15,6 +20,8 @@ func _ready() -> void:
 		$MeshInstance3D.mesh.material.emission = Color.GREEN
 	)
 	_randomise_tuning_parameters()
+	if movement_positions.size() > 1:
+		move_timer.timeout.connect(_move_to_next_position)
 
 func _randomise_tuning_parameters() -> void:
 	if data:
@@ -22,3 +29,10 @@ func _randomise_tuning_parameters() -> void:
 		data.target_cycles = randf_range(0.5, 5.0)
 		data.target_amplitude = randf_range(0.2, 1.0)
 		data.target_phase = randf_range(0.0, 1.99)
+
+func _move_to_next_position() -> void:
+	current_movement_position = (current_movement_position + 1) % movement_positions.size()
+	var tween = create_tween()
+	tween.set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(self, "global_position:y", movement_positions[current_movement_position].x, 1)
+	tween.parallel().tween_property(self, "global_position:z", movement_positions[current_movement_position].y, 1)
