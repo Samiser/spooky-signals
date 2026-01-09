22e824df7bb6834a1faad3de27d3fe3b8a9617e1
wookbee <159189510+wookbee@users.noreply.github.com>
2026-01-09 20:18:59 +0000

    fixed issues with point light size

--- entities/point_light/point_light.gd
+++ entities/point_light/point_light.gd
@@ -8,9 +8,9 @@ func _ready() -> void:
 	light_color = func_godot_properties.get("colour", Color.WHITE)
 	start_light_energy = func_godot_properties.get("energy", 1.0)
 	light_energy = start_light_energy
-	light_size = func_godot_properties.get("size", 10.0)
+	omni_range = func_godot_properties.get("size", 10.0)
 	shadow_enabled = func_godot_properties.get("shadows", false)
-	shadow_blur = 0.0
+	#shadow_blur = 0.0
 	flicker_amount = func_godot_properties.get("flicker_amount", 0.0)
 	light_volumetric_fog_energy = func_godot_properties.get("fog_energy", 0.0)
 	request_ready()
