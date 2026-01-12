af29d5febe635dae4556e7a3bcadca89cc807d65
wookbee <159189510+wookbee@users.noreply.github.com>
2026-01-12 15:09:41 +0000

    added player receiving signals from entities

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,11 +1,16 @@
-extends CharacterBody3D
+#extends CharacterBody3D
+extends Reciever
 class_name Player
+
+@onready var character_body: CharacterBody3D = $"."
 
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 var speed: int = 4
 var jump_speed: int = 4
 var mouse_sensitivity: float = 0.002
 var is_zoomed : bool = false
+var shake_time := 0.0
+var shake_magnitude := 128.0
 
 var interact_distance: float = 4.0
 
@@ -17,22 +22,27 @@ var current_interactable: Node3D
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	connect_senders("player", signal_recieved)
 
 func _physics_process(delta):
-	velocity.y += -gravity * delta
+	character_body.velocity.y += -gravity * delta
 	if !interacting:
 		var input = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
-		var movement_dir = transform.basis * Vector3(input.x, 0, input.y)
-		velocity.x = movement_dir.x * speed
-		velocity.z = movement_dir.z * speed
+		var movement_dir = character_body.transform.basis * Vector3(input.x, 0, input.y)
+		character_body.velocity.x = movement_dir.x * speed
+		character_body.velocity.z = movement_dir.z * speed
 		
-		if is_on_floor() and Input.is_action_just_pressed("jump"):
-			velocity.y = jump_speed
+		if character_body.is_on_floor() and Input.is_action_just_pressed("jump"):
+			character_body.velocity.y = jump_speed
 	else:
-		velocity.x = 0
-		velocity.z = 0
-		
-	move_and_slide()
+		character_body.velocity.x = 0
+		character_body.velocity.z = 0
+	
+	if shake_time > 0.0:
+		character_body.velocity += Vector3(randf_range(-1.0, 1.0), 0.0, randf_range(-1.0, 1.0)) * shake_time * shake_magnitude * delta
+		shake_time -= delta
+	
+	character_body.move_and_slide()
 
 func _process(delta: float) -> void:
 	_set_crosshair_visibility()
@@ -63,7 +73,7 @@ func toggle_zoom() -> void:
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
-		rotate_y(-event.relative.x * mouse_sensitivity)
+		character_body.rotate_y(-event.relative.x * mouse_sensitivity)
 		$Camera3D.rotate_x(-event.relative.y * mouse_sensitivity)
 		$Camera3D.rotation.x = clampf($Camera3D.rotation.x, -deg_to_rad(70), deg_to_rad(70))
 	
@@ -93,7 +103,7 @@ func _looking_at_interactable() -> Node3D:
 	var from := cam.global_transform.origin
 	var to := from + (-cam.global_transform.basis.z) * interact_distance
 
-	var space_state := get_world_3d().direct_space_state
+	var space_state := character_body.get_world_3d().direct_space_state
 	var query := PhysicsRayQueryParameters3D.create(from, to)
 	query.exclude = [self]
 
@@ -114,3 +124,23 @@ func _try_interact():
 	if interactable:
 		interactable.interact(self)
 		current_interactable = interactable
+
+func signal_recieved(parameters: String) -> void:
+	var param_list : PackedStringArray = parameters.split(', ', false)
+	for parameter in param_list:
+		match parameter:
+			"player_gravity_on":
+				gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+			"player_gravity_off":
+				gravity = 0.0
+			"player_gravity_half":
+				gravity = ProjectSettings.get_setting("physics/3d/default_gravity") / 2.0
+			"player_shake_mini":
+				shake_time = 1.0
+				shake_magnitude = 86
+			"player_shake_mid":
+				shake_time = 2.0
+				shake_magnitude = 132
+			"player_shake_big":
+				shake_time = 2.2
+				shake_magnitude = 256
